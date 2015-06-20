0b61f0be16bdabf4380ec07ec5aa540c6b2d1f24
Ubuntu <ubuntu@ip-172-31-23-107.us-west-2.compute.internal>
2015-06-20 22:34:37 +0000

    Ingestion Final Working

---
+++ ingestion/datagen.py
@@ -0,0 +1,335 @@
+##############################################################################################
+#
+# Script to generate user activity stream
+# Parameters:
+#   -sid <starting userid number>
+#   -eid <ending userid number>
+#   -d <number of days to produce activities for starting from today going back>
+#
+##############################################################################################
+
+__author__ = 'K'
+
+from faker import Factory
+from datetime import datetime, timedelta
+import argparse
+from random import randint
+import random
+import math
+
+# Read location information of all cities in california
+def read_locations():
+    locations = []
+    # Open locations.csv file
+    # Header: ZipCode,Latitude,Longitude,City,State,County
+    with open('Locations.csv', 'rU') as f:
+        content = f.readlines()
+    header = None
+    rownum = 0
+    # Loop through all the lines in file
+    for row in content:
+        # Save header line
+        if rownum == 0:
+            header = row.strip().split(',')
+        # For non header line, parse out fields
+        else:
+            colnum = 0
+            location_data = {}
+            # Loop through fields and create location_data structure
+            for col in row.strip().split(','):
+                location_data[header[colnum]] = col
+                colnum += 1
+            locations.append(location_data)
+        rownum += 1
+    # Return all the locations
+    return locations
+
+# Giver current time return what even user is doing based on schedule created
+def create_event(current_time, schedule, user_id, user_location):
+    # If no activity then set to IDLE
+    activityType = 'IDLE'
+
+    # If user has running activity that day
+    if schedule['hasRunningActivity']:
+        # If current time is within running activity, set activity type to RUNNING
+        if schedule['runningStart'] <= current_time <= schedule['runningEnd']:
+            activityType = 'RUNNING'
+    # If user has cycling activity that day
+    if schedule['hasCyclingActivity']:
+        # If current time is within cycling activity, set activity type to CYCLING
+        if schedule['cyclingStart'] <= current_time <= schedule['cyclingEnd']:
+            activityType = 'CYCLING'
+    # If user has walkning activity that day
+    if schedule['hasWalkingActivity']:
+        # If current time is within first walking activity start and last activity end
+        if schedule['walkingStart'] <= current_time <= schedule['walkingEnd']:
+            # Go through all walking activities and set activity type to WALKING
+            # if current time falls under the range
+            for i in range(0, schedule['totalTimesWalked']):
+                if schedule['walkingStart' + `i`] <= current_time <= schedule['walkingEnd' + `i`]:
+                    activityType = 'WALKING'
+    # If current time is before wakeup or after sleep set activity type to SLEEPING
+    if current_time > schedule['sleep'] or current_time < schedule['wakeup']:
+        activityType = 'SLEEPING'
+
+    # Construct json for a given activity details
+    return '{{"uuid": {0:08d}, "timestamp": {1}, "activity": "{2}", "latitude": {3}, "longitude": {4}}}'.format(
+        user_id,
+        timestamp_serializable(current_time),
+        activityType,
+        user_location['Latitude'],
+        user_location['Longitude']
+    )
+
+# Create random schedule for a given date
+def create_schedule(fake, dt):
+    # Get random wakeup time within morning time range
+    wakeup_time = get_wakeup_time(fake, dt)
+    # Get random sleep time within night time range
+    sleep_time = get_sleep_time(fake, dt)
+    distribution = randint(0, 9)
+    schedule = {}
+
+    # Start with no activity on the day
+    schedule['hasRunningActivity'] = False
+    schedule['hasCyclingActivity'] = False
+    schedule['hasWalkingActivity'] = False
+
+    # From distribution set certain activities based on probability
+    # Max 10% people cycle on a given day
+    if distribution == 0:
+        add_cycling_activity(fake, schedule, dt, wakeup_time, sleep_time)
+    # Max 20% people run on a given day
+    elif distribution >= 1 and distribution <= 2:
+        add_running_activity(fake, schedule, dt, wakeup_time, sleep_time)
+    # Max 1% people run and cycle on a given day (10% of a 10%)
+    elif distribution == 3:
+        distribution = randint(0, 9)
+        if distribution == 0:
+            add_cycling_activity(fake, schedule, dt, wakeup_time, sleep_time)
+            add_running_activity(fake, schedule, dt, wakeup_time, sleep_time)
+
+    # Max 90% people walk on a given day
+    distribution = randint(0, 9)
+    if distribution < 9:
+        add_walking_activity(schedule, wakeup_time, sleep_time)
+
+    # Set wakeup and sleep time in schedule
+    schedule['wakeup'] = wakeup_time
+    schedule['sleep'] = sleep_time
+    return schedule
+
+# Create walking activities throughout the day
+def add_walking_activity(schedule, wakeup_time, sleep_time):
+    # Only add walking activity after few hours from waking up
+    current_time = wakeup_time+timedelta(hours=randint(1, 3))
+    # Start with no activity on that day
+    schedule['hasWalkingActivity'] = False
+    # Expected number of walking activities are 0-5
+    expectedNumberOfTimesWalked = randint(0, 5)
+    # Start with 0 number of walking activities on that day
+    totalNumberOfTimesWalked = 0
+
+    # Create walking activites based on expected random number of activities
+    for i in range(0, expectedNumberOfTimesWalked):
+        # Start walking activity somewhere within 2 hours of given current time
+        walking_start = current_time + timedelta(hours=randint(0,1), minutes=randint(0,59))
+
+        # If this is first walking activity in the day, note that
+        if i == 0:
+            schedule['hasWalkingActivity'] = True
+            schedule['walkingStart'] = walking_start
+
+        distribution = randint(0, 9)
+        # 70% of walks will be shorter than 30 mins
+        if distribution < 7:
+            walking_end = walking_start + timedelta(minutes=30)
+        # 30% of walks will be upto 90 mins
+        else:
+            walking_end = walking_start + timedelta(minutes=90)
+
+        # If walking ends before sleep time then only consider it, and add to schedule
+        if walking_end < (sleep_time + timedelta(hours=-1)):
+            schedule['walkingStart' + `i`] = walking_start
+            schedule['walkingEnd' + `i`] = walking_end
+            totalNumberOfTimesWalked += 1
+            # Set the walking activity end of whole day
+            schedule['walkingEnd'] = walking_end
+        else:
+            break
+
+        # Next walking activity should be within next 4 hours of current walking activity
+        current_time = walking_end + timedelta(hours=randint(0,3), minutes=randint(0,59))
+
+    # Set how many times walking on the day
+    schedule['totalTimesWalked'] = totalNumberOfTimesWalked
+
+# Create running activity for the day, only one activity per day
+def add_running_activity(fake, schedule, dt, wakeup_time, sleep_time):
+    # Start with no running activity for the day
+    hasRunningActivity = False
+    running_start_time = None
+    running_end_time = None
+
+    # If this is a weekday, probability of running is 70%
+    if dt.weekday() < 5:
+        distribution = randint(0, 9)
+        if distribution < 7:
+            # Duration of running activity should be upto 2 hours
+            running_start_time = get_running_start_time(fake, wakeup_time, sleep_time)
+            running_end_time = running_start_time + timedelta(hours=randint(0,1), minutes=randint(0,59))
+            hasRunningActivity = True
+    # If this is weekend, probability of running is 90%
+    else:
+        distribution = randint(0, 9)
+        if distribution < 9:
+            # Duration of running activity should be upto 3 hours
+            running_start_time = get_running_start_time(fake, wakeup_time, sleep_time)
+            running_end_time = running_start_time + timedelta(hours=randint(0,2), minutes=randint(0,59))
+            hasRunningActivity = True
+
+    # Set activity in schedule
+    schedule['hasRunningActivity'] = hasRunningActivity
+    schedule['runningStart'] = running_start_time
+    schedule['runningEnd'] = running_end_time
+
+# Create cycling activity for the day, only one activity per day
+def add_cycling_activity(fake, schedule, dt, wakeup_time, sleep_time):
+    # Start with no cycling activity for the day
+    hasCyclingActivity = False
+    cycling_start_time = None
+    cycling_end_time = None
+
+    # If this is a weekday, probability of cycling is 70%
+    if dt.weekday() < 5: # week day
+        distribution = randint(0, 9)
+        if distribution < 7:
+            # Duration of cycling activity should be upto 2 hours
+            cycling_start_time = get_cycling_start_time(fake, wakeup_time, sleep_time)
+            cycling_end_time = cycling_start_time + timedelta(hours=randint(0,1), minutes=randint(0,59))
+            hasCyclingActivity = True
+    # If this is weekend, probability of running is 90%
+    else:
+        distribution = randint(0, 9)
+        if distribution < 9:
+            # Duration of cycling activity should be upto 3 hours
+            cycling_start_time = get_cycling_start_time(fake, wakeup_time, sleep_time)
+            cycling_end_time = cycling_start_time + timedelta(hours=randint(0,2), minutes=randint(0,59))
+            hasCyclingActivity = True
+    schedule['hasCyclingActivity'] = hasCyclingActivity
+    schedule['cyclingStart'] = cycling_start_time
+    schedule['cyclingEnd'] = cycling_end_time
+
+# Get running start time based on probability morning vs. evening
+def get_running_start_time(fake, wakeup_time, sleep_time):
+    distribution = randint(0, 9)
+    # 30% people run in morning
+    if distribution < 3:
+        return fake.date_time_between(start_date=wakeup_time+timedelta(hours=1), end_date=wakeup_time+timedelta(hours=4))
+    # 70% people run in evening
+    else:
+        return fake.date_time_between(start_date=sleep_time+timedelta(hours=-5), end_date=sleep_time+timedelta(hours=-1))
+
+# Get cycling start time based on probability morning vs. evening
+def get_cycling_start_time(fake, wakeup_time, sleep_time):
+    distribution = randint(0, 9)
+    # 30% people cycle in morning
+    if distribution < 3:
+        return fake.date_time_between(start_date=wakeup_time+timedelta(hours=1), end_date=wakeup_time+timedelta(hours=4))
+    # 70% people cycle in evening
+    else:
+        return fake.date_time_between(start_date=sleep_time+timedelta(hours=-5), end_date=sleep_time+timedelta(hours=-1))
+
+# Get start of a day, 0 AM
+def get_start_of_day(dt):
+    return datetime(dt.year, dt.month, dt.day)
+
+# Get end of a day, 11:59:59PM
+def get_end_of_day(dt):
+    return get_start_of_day(dt) + timedelta(hours=23, minutes=59, seconds=59)
+
+# Get random wakeup time between 5-10 AM
+def get_wakeup_time(fake, dt):
+    start_of_day = get_start_of_day(dt)
+    return fake.date_time_between(start_date=start_of_day+timedelta(hours=5), end_date=start_of_day+timedelta(hours=10))
+
+# Get random sleep time between 7PM-midnight
+def get_sleep_time(fake, dt):
+    return fake.date_time_between(start_date=get_start_of_day(dt)+timedelta(hours=19), end_date=get_end_of_day(dt))
+
+# Get formated string for date
+def datetime_serializable(dt):
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+# Get timestamp of the date
+def timestamp_serializable(dt):
+    return dt.strftime("%s")
+
+# Given a location in lat,long produce near by location in given radius
+def get_random_location_around(location, radius):
+    radius_in_degree = radius/111300
+    u = float(random.uniform(0.0, 0.1))
+    v = float(random.uniform(0.0, 0.1))
+    w = radius_in_degree * math.sqrt(u)
+    t = 2 * math.pi * v
+    x = w * math.cos(t)
+    y = w * math.sin(t)
+    return {
+        'Latitude': location['Latitude'] + x,
+        'Longitude': location['Longitude'] + y
+    }
+
+# Main function
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sid", help="start user id")
+    parser.add_argument("-eid", help="end user id")
+    parser.add_argument("-d", help="number of days to produce data for")
+    args = parser.parse_args()
+
+    fake = Factory.create()
+    start_user_id = int(args.sid)
+    end_user_id = int(args.eid)
+    number_of_days = int(args.d)
+
+    # Read city locations
+    locations = read_locations()
+
+    # Produce activity stream for each user between start and end user id
+    # supplied in command line
+    for user_id in range(start_user_id, end_user_id):
+        # Create file with userid filename to write activity data
+        user_file = open("data/{0:08d}.dat".format(user_id), 'w')
+        events = []
+        # Randomly assign location to user
+        user_location = get_random_location_around({
+            'Latitude': float(locations[randint(0, len(locations) -1)]['Latitude']),
+            'Longitude': float(locations[randint(0, len(locations) -1)]['Longitude'])
+        }, 10000)
+        days = number_of_days
+        # Go through each day and produce activity stream
+        # Do this for number of days supplied in command line
+        while days >= 0:
+            date_of_activity = datetime.now() - timedelta(hours=days)
+            # Create day schedule
+            schedule = create_schedule(fake, datetime.now())
+            # Get start of the day, 0AM
+            current_time = get_start_of_day(datetime.now())
+            # Get end of the day, 11:59:59PM
+            end_of_day = get_end_of_day(current_time)
+            # Get user activity for the day every 5 mins
+            # write to file and print
+            while current_time < end_of_day:
+                event = create_event(current_time, schedule, user_id, user_location)
+                print event
+                user_file.write(event + '\n')
+                events.append(event)
+                current_time = current_time + timedelta(minutes=5)
+            days -= 1
+        # Close the file once done with user
+        user_file.close()
+
+
+
+
--- ingestion/kafka_producer.py
+++ ingestion/kafka_producer.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python
 __author__ = 'kr9'
 import time
-import csv
-
+import glob, os
 from kafka.client import KafkaClient
 from kafka.producer import SimpleProducer
 
 class kafkaProducer(object):
-
-    
     
     def __init__(self, addr):
         self.client = KafkaClient(addr) # kafka host address
         self.producer = SimpleProducer(self.client)
 
-    def createData(self, activityid, typeid, uuid, timestamp, loclat, loclon): # pass the fields
-        
-        if activityid: #checks if any more records available
-            stringRow = "%s,%s,%s,%s,%s,%s" % (activityid, typeid, uuid, timestamp, loclat, loclon)#converts each row to string
-            self.producer.send_messages("messages", stringRow)#send message named messages
-            print(stringRow)
-            timestamp1 = list(time.localtime()[0:6])
-            print timestamp1
-
     def readAndStream(self):
 
-        with open('data/sampledata.csv', "rU") as csvfile:
-            filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in filereader:
-                self.createData(row[0],row[1],row[2],row[3],row[4],row[5])
-                #print row
-                time.sleep(0.03)
-        filereader.close()
+        os.chdir("data")
+        for file in glob.glob("*.dat"):
+            with open(file, 'rU') as f:
+                content = f.readlines()
+            for row in content:
+                self.producer.send_messages("messages", row)#send message named messages
+                print row
+                timestamp1 = list(time.localtime()[0:6])
+                print timestamp1
+                time.sleep(0.01)
 
 activityProducer = kafkaProducer("localhost:9092")
 activityProducer.readAndStream()
