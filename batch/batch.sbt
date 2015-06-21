cafff502a1bea9aa4e575dfbd09d6c480f3ba100
Ubuntu <ubuntu@ip-172-31-23-107.us-west-2.compute.internal>
2015-06-21 00:16:19 +0000

    sbt and scala file changed

--- batch/batch.sbt
+++ batch/batch.sbt
@@ -1,12 +1,22 @@
-name := "activitytest"
+name := "batch_activity"
 
 version := "1.0"
 
 scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
-  "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0-alpha1", 
-  "org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
-  "org.apache.spark" %% "spark-sql" % "1.2.0"
+"org.apache.spark" %% "spark-core" % "1.3.0" % "provided",
+"org.apache.spark" %% "spark-sql" % "1.3.0" % "provided",
+"org.apache.spark" % "spark-streaming_2.10" % "1.3.0" % "provided",
+"org.apache.spark" % "spark-streaming-kafka_2.10" % "1.3.0"
+"com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0-alpha1", 
 )
 
+mergeStrategy in assembly := {
+  case m if m.toLowerCase.endsWith("manifest.mf")          => MergeStrategy.discard
+  case m if m.toLowerCase.matches("meta-inf.*\\.sf$")      => MergeStrategy.discard
+  case "log4j.properties"                                  => MergeStrategy.discard
+  case m if m.toLowerCase.startsWith("meta-inf/services/") => MergeStrategy.filterDistinctLines
+  case "reference.conf"                                    => MergeStrategy.concat
+  case _                                                   => MergeStrategy.first
+}
--- batch/batch_activity.scala
+++ batch/batch_activity.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.SQLContext
 import com.datastax.spark.connector._
 import com.datastax.spark.connector._
 import scala.collection.JavaConversions._
+import com.datastax.driver.core.utils._
 
 
 object batch_activity
@@ -13,7 +14,9 @@ object batch_activity
   {
     // -- Initialization --
     val host = "127.0.0.1"
-    val filepath = "hdfs://ec2-52-26-58-1.us-west-2.compute.amazonaws.com:9000/user/react/history/activityjson.json"
+    val filepath = "hdfs://ec2-52-26-58-1.us-west-2.compute.amazonaws.com:9000/user/react/history/hdfs_messages_20150620235308.dat"
+
+
     
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", host)
     val sc = new SparkContext(conf)
@@ -21,16 +24,16 @@ object batch_activity
     // load JSON files and save as table
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-
-    
     val tempact1 = sqlContext.jsonFile(filepath)
     //val tempact1 = sqlContext.jsonFile("hdfs://ec2-52-26-58-1.us-west-2.compute.amazonaws.com:9000/user/react/history/activityjson.json")
 
+    tempact1.printSchema()
+    
     tempact1.registerTempTable("activity")
 
     // Insert test data into Activity test table
 
-    val list = sqlContext.sql("SELECT ActivityID FROM activity")
+    val list = sqlContext.sql("SELECT * FROM activity")
     list.collect.foreach(println)
 
 
@@ -38,35 +41,50 @@ object batch_activity
     //list.show() // Shows list on command line
 
     // Map DataFrame to an RDD of case classes
-    case class write_tempactivity(activity_id: String)
-    val write_Tempactivity = list.map( r => write_tempactivity(r(0).toString ))
-
-
-
-    // Write RDD of case classes into the Cassandra table
-    write_Tempactivity.saveToCassandra("activitydb", "tempactivity")
-
-
-    println("ActivityID sent to Cassandra")
-
-
-    //Create new case class and insert data into activity_by_user table
-
-    val alldata = sqlContext.sql("SELECT * FROM activity")
-    alldata.collect.foreach(println)
-
-    println("Data from Activity table////////////////////////////////////////")
-
-      // Map DataFrame to an RDD of case classes
-    case class write_to_master_activity(activity_id: String, lat: Double, lon: Double, time: String, activity_type: String, user_id: String)
     
-    println("case class created////////////////////////////////////////")
+
+    // case class write_tempactivity(activity_id: String)
+    // val write_Tempactivity = list.map( r => write_tempactivity(r(0).toString ))
 
 
-    val write_to_Master_Activity = alldata.map( s => write_to_master_activity(s(0).toString, s(1).toString.toDouble, s(2).toString.toDouble, s(3).toString,s(4).toString, s(5).toString ))
 
-    // Write RDD of case classes into the Cassandra table
-    write_to_Master_Activity.saveToCassandra("activitydb", "master_activity")
+    // // Write RDD of case classes into the Cassandra table
+    // write_Tempactivity.saveToCassandra("activitydb", "tempactivity")
+
+
+    // println("ActivityID sent to Cassandra")
+
+
+    // //Create new case class and insert data into activity_by_user table
+
+    // val alldata = sqlContext.sql("SELECT * FROM activity")
+    // alldata.collect.foreach(println)
+
+    // println("Data from Activity table////////////////////////////////////////")
+
+    //   // Map DataFrame to an RDD of case classes
+    // case class write_to_master_activity(activity_id: String, 
+    //     lat: Double, 
+    //     lon: Double, 
+    //     time: String, 
+    //     activity_type: String, 
+    //     user_id: String,
+    //     name: String,
+    //     zip: int)
+    
+    // println("case class created////////////////////////////////////////")
+
+
+    // val write_to_Master_Activity = alldata.map( s => write_to_master_activity(UUIDs.random(),
+    //     s(0).toString, 
+    //     s(1).toString.toDouble, 
+    //     s(2).toString.toDouble, 
+    //     s(3).toString,
+    //     s(4).toString, 
+    //     s(5).toString ))
+
+    // // Write RDD of case classes into the Cassandra table
+    // write_to_Master_Activity.saveToCassandra("activitydb", "master_activity")
 
 
     }
