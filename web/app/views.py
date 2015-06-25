e2a87a7efdbf041e0263711f61fee5a2511bf079
K <K@KMBPR.local>
2015-06-24 23:55:42 -0700

    Web folder changes. Added coordinates

--- web/app/views.py
+++ web/app/views.py
@@ -14,7 +14,7 @@ def get_users(one=False):
         users.append(map_user(row))
     return users
 
-def get_user(user_id, one=True):
+def get_user(user_id):
     row = session.execute("SELECT * FROM user WHERE user_id = '{0}'".format(user_id))
     return map_user(row[0])
 
@@ -34,29 +34,11 @@ def search_users_by_id(user_id):
 
 @app.route('/users/<zip>/<activity_type>')
 def search_users_by_zip_activity_type(zip, activity_type):
-    rows = session.execute("SELECT * FROM activity_by_user WHERE zip='{1}' AND activity_type='{2}'".format(zip, activity_type))
+    rows = session.execute("SELECT * FROM activity_by_user WHERE zip='{0}' AND activity_type='{1}'".format(zip, activity_type))
     users = []
-    for row in record_list:
+    for row in rows:
         users.append(map_user_activity(row))
     return jsonify({"usersStats": users})
-
-@app.route("/map")
-def map():    
-    # map_1 = folium.Map(location=[45.372, -121.6972], zoom_start=12,
-    #                tiles='Stamen Terrain')
-    # map_1.simple_marker([45.3288, -121.6625], popup='Mt. Hood Meadows')
-    # map_1.simple_marker([45.3311, -121.7113], popup='Timberline Lodge')
-    # map_1.create_map(path='~/templates/map.html')
-    '''
-    lat = "SELECT lat FROM {} WHERE activity_id= '123456802'".format(table)
-    lon = "SELECT lon FROM {} WHERE activity_id= %s".format(table)
-    act='123456802'
-    lat1 = session.execute(lat,act)
-    lon1 = session.execute(lon,act)
-    print lat1
-    print lon1
-    '''
-    return render_template("osm.html")
 
 @app.route('/')
 @app.route('/index')
