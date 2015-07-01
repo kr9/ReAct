9ab76c1bcadba963eafeb0ce08d67801793335c9
Ubuntu <ubuntu@ip-172-31-25-20.us-west-2.compute.internal>
2015-07-01 03:09:38 +0000

    Fixed number of items on map

--- web/app/views.py
+++ web/app/views.py
@@ -7,8 +7,11 @@ cluster = Cluster(['52.26.58.1'])
 session = cluster.connect()
 session.set_keyspace("activitydb")
 
-def get_users(one=False):
-    rows = session.execute("SELECT * FROM user")
+def get_users(limited=True):
+    if limited == True:
+        rows = session.execute("SELECT * FROM user LIMIT 2000")
+    else:
+        rows = session.execute("SELECT * FROM user")
     users = []
     for row in rows:
         users.append(map_user(row))
@@ -26,11 +29,11 @@ def map_user(row):
     return {"user_id" : row.user_id, "name" : row.name, "zip" : row.zip, "lat": row.lat, "lon": row.lon }
 
 def map_user_activity(row):
-    return {'user_id' : row.user_id, 'zip' : row.zip, 'activity_type' : row.activity_type, 'duration' : row.duration, "lat": row.lat, "lon": row.lon }
+    return {'user_id' : row.user_id, 'zip' : row.zip, 'activity_type' : row.activity_type, "lat": row.lat, "lon": row.lon }
 
 @app.route('/users')
 def all_users():
-    return jsonify({"users" : get_users()})
+    return jsonify({"users" : get_users(False)})
 
 @app.route('/users/<user_id>')
 def search_users_by_id(user_id):
@@ -42,7 +45,7 @@ def search_userstats_by_id(user_id, zip, activity_type):
 
 @app.route('/users/<zip>/<activity_type>')
 def search_users_by_zip_activity_type(zip, activity_type):
-    rows = session.execute("SELECT * FROM activity_by_user WHERE zip='{0}' AND activity_type='{1}'".format(zip, activity_type))
+    rows = session.execute("SELECT * FROM activity_avg WHERE zip='{0}' AND activity_type='{1}'".format(zip, activity_type))
     users = []
     for row in rows:
         users.append(map_user_activity(row))
